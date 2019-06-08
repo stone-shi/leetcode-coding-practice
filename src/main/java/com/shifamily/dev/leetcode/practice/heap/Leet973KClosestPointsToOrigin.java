e54489e7f160309c1a3cc78e35ace7e19647e520
Stone Shi <stone@shifamily.com>
2019-06-07 21:39:16 -0700

    added Leetcode 973

--- src/main/java/com/shifamily/dev/leetcode/practice/heap/Leet973KClosestPointsToOrigin.java
+++ src/main/java/com/shifamily/dev/leetcode/practice/heap/Leet973KClosestPointsToOrigin.java
@@ -1,5 +1,12 @@
 package com.shifamily.dev.leetcode.practice.heap;
 
+import com.shifamily.dev.leetcode.practice.BasicStudy;
+import com.shifamily.dev.leetcode.practice.CaseRunner;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /*
 973. K Closest Points to Origin
 Medium
@@ -34,11 +41,90 @@ Note:
 -10000 < points[i][0] < 10000
 -10000 < points[i][1] < 10000
  */
-public class Leet973KClosestPointsToOrigin {
+public class Leet973KClosestPointsToOrigin extends BasicStudy {
 
-    public int[][] kClosest(int[][] points, int K) {
+    public Leet973KClosestPointsToOrigin() {
 
-        return null;
+        int[][] case1P1 = {{1,3},{-2,2}};
+        int case1P2 = 1;
+
+        Object[] p1 = new Object[2];
+        p1[0] = case1P1;
+        p1[1] = case1P2;
+
+        int[][] answer1 = {{-2, 2}};
+
+        addParameterAndAnswer(p1, answer1, false);
+
+    }
+
+
+
+    //直接利用排序，然后取前K。到原点距离计算  sqrt(x^2 + y^2)，排序时不必考虑 平方根
+    @CaseRunner
+    public int[][] kClosestSort(int[][] points, int K) {
+
+        Arrays.sort(points,  Comparator.comparingInt(a -> a[0] * a[0] + a[1] * a[1]));
+
+        return Arrays.copyOfRange(points, 0, K );
+    }
+
+
+    //利用优先队列，始终保持K
+    @CaseRunner
+    public int[][] kClosestHeap(int[][] points, int K) {
+
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> b[0]*b[0] + b[1]*b[1] - a[0]*a[0] - a[1]*a[1]);
+
+        for (int i = 0; i < points.length ; i++) {
+            queue.offer(points[i]);
+            if (queue.size() > K)
+                queue.poll();
+        }
+
+        int[][] res = new int[K][];
+        for (int i = 0; i < K; i++)
+            res[i] = queue.poll();
+
+        return res;
+
+    }
+
+    //Quick select算法
+    @CaseRunner
+    public int[][] kClosestQuickSelect(int[][] points, int K) {
+
+        int l = 0;
+        int r = points.length - 1;
+
+        while (l < r) {
+            int mid = helper(points, l, r);
+            if (mid < K)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return Arrays.copyOfRange(points, 0, K);
+    }
+
+    private int helper( int[][]  points, int l, int r){
+        int[] pivot = points[l]; //"随机"选择的 pivot point
+
+        while ( l < r){
+            //先 r 减小，找到第一个 小于 pivot的
+            while (l < r && compare(points[r], pivot) >= 0 ) r--;
+            points[l] = points[r];
+            //l 增加，找到第一个 大于 Pivot的
+            while (l < r && compare(points[l], pivot) <= 0) l++;
+            points[r] = points[l];
+        }
+        points[l] = pivot;
+        return l;
+    }
+
+
+    private int compare(int[] a, int[] b){
+        return a[0] * a[0] + a[1] * a[1] - b[0] * b[0] - b[1] * b[1];
     }
 
 
--- src/main/java/com/shifamily/dev/leetcode/practice/heap/Leet973KClosestPointsToOrigin.md
+++ src/main/java/com/shifamily/dev/leetcode/practice/heap/Leet973KClosestPointsToOrigin.md
@@ -40,30 +40,97 @@ Note:
 -10000 < points[i][0] < 10000
 -10000 < points[i][1] < 10000
 
-# 解法
+# 解法Log
+到原点距离计算  sqrt(x^2 + y^2)，排序时不必考虑平方根  
 
 ## 解法1
-
+最简单的解法，直接对整个points数组排序
 
 ### 复杂度
-时间复杂度 O(N)
-空间复杂度 O(N)
+时间复杂度 O(NLogN)
+空间复杂度 O(1)
 
 
 ### 代码
 ```Java
-
+   public int[][] kClosestSort(int[][] points, int K) {
+        Arrays.sort(points,  Comparator.comparingInt(a -> a[0] * a[0] + a[1] * a[1]));
+        return Arrays.copyOfRange(points, 0, K );
+    }
 ```
 
 ## 解法2
-
+这次利用 max heap来排序，heap内最多时候只有 k个point，所以比全排序快
 
 ### 复杂度
-时间复杂度 O(N)
-空间复杂度 O(N)
+时间复杂度 O(NLogK)
+空间复杂度 O(K)
 
 
 ### 代码
 ```Java
+   public int[][] kClosestHeap(int[][] points, int K) {
+
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> b[0]*b[0] + b[1]*b[1] - a[0]*a[0] - a[1]*a[1]);
+
+        for (int i = 0; i < points.length ; i++) {
+            queue.offer(points[i]);
+            if (queue.size() > K)
+                queue.poll();
+        }
+
+        int[][] res = new int[K][];
+        for (int i = 0; i < K; i++)
+            res[i] = queue.poll();
+
+        return res;
+
+    }
+```
+## 解法3
+这个是 quick select算法，是选k th 元素最快的算法。
+
+### 复杂度
+时间复杂度 平均O(N)，worst O(N^2)
+空间复杂度 O(1)
+
+
+### 代码
+```Java
+    public int[][] kClosestQuickSelect(int[][] points, int K) {
+
+        int l = 0;
+        int r = points.length - 1;
+
+        while (l < r) {
+            int mid = helper(points, l, r);
+            if (mid < K)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return Arrays.copyOfRange(points, 0, K);
+    }
+
+    private int helper( int[][]  points, int l, int r){
+        int[] pivot = points[l]; //"随机"选择的 pivot point
+
+        while ( l < r){
+            //先 r 减小，找到第一个 小于 pivot的
+            while (l < r && compare(points[r], pivot) >= 0 ) r--;
+            points[l] = points[r];
+            //l 增加，找到第一个 大于 Pivot的
+            while (l < r && compare(points[l], pivot) <= 0) l++;
+            points[r] = points[l];
+        }
+        points[l] = pivot;
+        return l;
+    }
+
+
+    private int compare(int[] a, int[] b){
+        return a[0] * a[0] + a[1] * a[1] - b[0] * b[0] - b[1] * b[1];
+    }
 
 ```
+
