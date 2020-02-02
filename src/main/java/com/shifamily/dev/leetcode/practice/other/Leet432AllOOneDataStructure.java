6d6f55cefff898f3adcef0be29df8028918cb030
Stone Shi <stone@shifamily.com>
2020-02-02 13:45:03 -0800

    added some questions

--- src/main/java/com/shifamily/dev/leetcode/practice/other/Leet432AllOOneDataStructure.java
+++ src/main/java/com/shifamily/dev/leetcode/practice/other/Leet432AllOOneDataStructure.java
@@ -8,6 +8,30 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+432 All O`one Data Structure
+Hard
+
+470
+
+61
+
+Add to List
+
+Share
+Implement a data structure supporting the following operations:
+
+Inc(Key) - Inserts a new key with value 1. Or increments an existing key by 1. Key is guaranteed to be a non-empty string.
+Dec(Key) - If Key's value is 1, remove it from the data structure. Otherwise decrements an existing key by 1. If the key does not exist, this function does nothing. Key is guaranteed to be a non-empty string.
+GetMaxKey() - Returns one of the keys with maximal value. If no element exists, return an empty string "".
+GetMinKey() - Returns one of the keys with minimal value. If no element exists, return an empty string "".
+Challenge: Perform all these in O(1) time complexity.
+
+Accepted
+25,810
+Submissions
+82,524
+ */
 @Slf4j
 public class Leet432AllOOneDataStructure extends BasicStudy {
 
